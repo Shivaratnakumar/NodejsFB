f4b73919725acfc7476e2d45174510784ba91879
Shivaratnakumar <shivaratnakumarpatil@gmail.com>
2016-11-21 13:14:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+angular.module('myApp', ['ngFacebook'])
+  .config(['$facebookProvider', function($facebookProvider) {
+    $facebookProvider.setAppId('764262530321266').setPermissions(['email','user_friends']);
+  }])
+  .run(['$rootScope', '$window', function($rootScope, $window) {
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    $rootScope.$on('fb.load', function() {
+      $window.dispatchEvent(new Event('fb.load'));
+    });
+  }])
+  .controller('myCtrl', ['$scope', '$facebook', function($scope, $facebook) {
+    $scope.$on('fb.auth.authResponseChange', function() {
+      $scope.status = $facebook.isConnected();
+      if($scope.status) {
+        $facebook.api('/me').then(function(user) {
+          $scope.user = user;
+        });
+      }
+    });
+
+    $scope.loginToggle = function() {
+      if($scope.status) {
+        $facebook.logout();
+      } else {
+        $facebook.login();
+      }
+    };
+
+    $scope.getFriends = function() {
+      if(!$scope.status) return;
+      $facebook.cachedApi('/me/friends').then(function(friends) {
+        $scope.friends = friends.data;
+      });
+    }
+  }])
+;
